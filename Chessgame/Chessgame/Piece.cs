16ec9fd173cb1cada23d93492dcad8c320772434
Linustronell <120012941+Linustronell@users.noreply.github.com>
2023-05-16 12:51:35 +0200

    Update Piece.cs

--- Chessgame/Chessgame/Piece.cs
+++ Chessgame/Chessgame/Piece.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualBasic.Devices;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
@@ -16,17 +17,17 @@ namespace Chessgame
         public Vector2 _piecePosition;
         public Texture2D _pieceTexture;
         public bool _isAlive;
-        public bool _hasMove;
+        public bool _hasMoved;
         public Rectangle _boundingBox;
         public Color _color;
 
-        public Piece(Vector2 piecePosition, Texture2D pieceTexture, bool isAlive, bool hasMove, string pieceName, string team)
+        public Piece(Vector2 piecePosition, Texture2D pieceTexture, bool isAlive, bool hasMoved, string pieceName, string team)
         {
             _pieceName = pieceName;
             _piecePosition = piecePosition;
             _pieceTexture = pieceTexture;
             _isAlive = isAlive;
-            _hasMove = hasMove;
+            _hasMoved = hasMoved;
             _team = team;
             _color = Color.White;
             if (team == "B")
@@ -45,17 +46,7 @@ namespace Chessgame
             return false;
         }
 
-        public void Move(float x, float y)
-        {
-            _piecePosition = new Vector2(x,y);
-            _boundingBox = new Rectangle((int)_piecePosition.X, (int)_piecePosition.Y, 100, 100);
-        }
-
-        public void Draw(SpriteBatch sb)
-        {
-            sb.Draw(_pieceTexture, _piecePosition, _color);
-        }
-        public List<Vector2> Move()
+        public List<Vector2> AvailableMoves()
         {
             List<Vector2> Moves = new List<Vector2>();
 
@@ -96,5 +87,10 @@ namespace Chessgame
             
             
         }
-    }
-}
+        
+
+        public void Draw(SpriteBatch sb)
+        {
+            sb.Draw(_pieceTexture, _piecePosition, _color);
+        }
+        
